c0d31a6a390b97815fa0083736f762b4b87a4c51
kalpa wijesooriya <kalpa.wijesooriya@yahoo.com>
2019-08-25 21:32:34 +0530

    Initialize the Project

---
+++ SISS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SISS.MvcApplication" Language="C#" %>
---
+++ SISS/Models/SISS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using SISS.Models;
+
+namespace SISS 
+{
+    public class SISS_Context : DbContext
+    {
+        public SISS_Context() : base("name=SISS_context")
+        {
+
+        }
+        public DbSet<Login> Login { get; set; }
+    }
+}
